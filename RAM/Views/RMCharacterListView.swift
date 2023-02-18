321018f85e91756ffeb14e2111bc5dc7bac7ac6d
Alex S.K <98609838+strel4k@users.noreply.github.com>
2023-02-18 16:29:13 +0300

    Showing characters

--- RAM/Views/RMCharacterListView.swift
+++ RAM/Views/RMCharacterListView.swift
@@ -39,6 +39,7 @@ final class RMCharacterListView: UIView {
         addSubviews(collectionView, spinner)
         addConstarits()
         spinner.startAnimating()
+        viewModel.delegate = self
         viewModel.fetchCharacters()
         setupCollectionView()
     }
@@ -64,13 +65,17 @@ final class RMCharacterListView: UIView {
     private func setupCollectionView() {
         collectionView.dataSource = viewModel
         collectionView.delegate = viewModel
-        DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
-            self.spinner.stopAnimating()
-            
-            self.collectionView.isHidden = false
-            UIView.animate(withDuration: 0.4) {
-                self.collectionView.alpha = 1
-            }
-        })
+
+    }
+}
+
+extension RMCharacterListView: RMCharacterListViewViewModelDelegate {
+    func didLoadInitialCharacters() {
+        spinner.stopAnimating()
+        collectionView.isHidden = false
+        collectionView.reloadData() // Initial fetch
+        UIView.animate(withDuration: 0.4) {
+            self.collectionView.alpha = 1
+        }
     }
 }
