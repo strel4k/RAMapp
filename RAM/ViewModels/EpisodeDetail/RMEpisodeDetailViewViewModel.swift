29cfca9eaecfc49218cfe8872b1b22d5f0c0d585
Alex S.K <98609838+strel4k@users.noreply.github.com>
2023-03-26 18:10:42 +0300

    Completed Episode details logic

--- RAM/ViewModels/EpisodeDetail/RMEpisodeDetailViewViewModel.swift
+++ RAM/ViewModels/EpisodeDetail/RMEpisodeDetailViewViewModel.swift
@@ -39,6 +39,13 @@ final class RMEpisodeDetailViewViewModel {
         self.endpointUrl = endpointUrl
     }
     
+    public func character(at index: Int) -> RMCharacter? {
+        guard let dataTuple = dataTuple else {
+            return nil
+        }
+        return dataTuple.characters[index]
+    }
+    
     // MARK: - Public
     
     
@@ -52,12 +59,18 @@ final class RMEpisodeDetailViewViewModel {
         
         let episode = dataTuple.episode
         let characters = dataTuple.characters
+        
+        var createdString = episode.created
+        if let date = RMCharacterInfoCollectionViewCellViewModel.dateFormatter.date(from: episode.created) {
+            createdString = RMCharacterInfoCollectionViewCellViewModel.shortDateFormatter.string(from: date)
+        }
+        
         cellViewModels = [
             .information(viewModels: [
                 .init(title: "Episode name", value: episode.name),
                 .init(title: "Air date", value: episode.air_date),
                 .init(title: "Episode", value: episode.episode),
-                .init(title: "Created", value: episode.created)
+                .init(title: "Created", value: createdString)
             ]),
             .characters(viewModel: characters.compactMap({ character in
                 return RMCharacterCollectionViewCellViewModel(
